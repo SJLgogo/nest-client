0784ab3e6316b627520b94ce1bb09b08359fd2e9
sjl <906320505@qq.com>
2023-02-10 12:28:12 +0800

    init

---
+++ proxy.conf.js
@@ -0,0 +1,18 @@
+/**
+ * For more configuration, please refer to https://angular.io/guide/build#proxying-to-a-backend-server
+ *
+ * 更多配置描述请参考 https://angular.cn/guide/build#proxying-to-a-backend-server
+ *
+ * Note: The proxy is only valid for real requests, Mock does not actually generate requests, so the priority of Mock will be higher than the proxy
+ */
+module.exports = {
+  /**
+   * The following means that all requests are directed to the backend `https://localhost:9000/`
+   */
+  // '/api': {
+  //   target: 'http://localhost:4000/',
+  //   secure: false, // Ignore invalid SSL certificates
+  //   changeOrigin: true,
+  //   pathRewrite: { '^/api': '' }
+  // }
+};
---
+++ src/app/routes/passport/login/login.component.ts
@@ -0,0 +1,137 @@
+import { HttpContext } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy, Optional } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StartupService } from '@core';
+import { ReuseTabService } from '@delon/abc/reuse-tab';
+import { ALLOW_ANONYMOUS, DA_SERVICE_TOKEN, ITokenService, SocialOpenType, SocialService } from '@delon/auth';
+import { SettingsService, _HttpClient } from '@delon/theme';
+import { environment } from '@env/environment';
+import { NzTabChangeEvent } from 'ng-zorro-antd/tabs';
+import { finalize } from 'rxjs';
+
+@Component({
+  selector: 'passport-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less'],
+  providers: [SocialService],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserLoginComponent implements OnDestroy {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private settingsService: SettingsService,
+    private socialService: SocialService,
+    @Optional()
+    @Inject(ReuseTabService)
+    private reuseTabService: ReuseTabService,
+    @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
+    private startupSrv: StartupService,
+    private http: _HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  // #region fields
+
+  form = this.fb.nonNullable.group({
+    userName: ['', [Validators.required, Validators.pattern(/^(admin|user)$/)]],
+    password: ['', [Validators.required, Validators.pattern(/^(ng\-alain\.com)$/)]],
+    mobile: ['', [Validators.required, Validators.pattern(/^1\d{10}$/)]],
+    captcha: ['', [Validators.required]],
+    remember: [true]
+  });
+  error = '';
+  type = 0;
+  loading = false;
+  username:string='';
+  password:string=''
+
+  // #region get captcha
+
+  count = 0;
+  interval$: any;
+
+  // #endregion
+
+  switch({ index }: NzTabChangeEvent): void {
+    this.type = index!;
+  }
+
+  getCaptcha(): void {
+    const mobile = this.form.controls.mobile;
+    if (mobile.invalid) {
+      mobile.markAsDirty({ onlySelf: true });
+      mobile.updateValueAndValidity({ onlySelf: true });
+      return;
+    }
+    this.count = 59;
+    this.interval$ = setInterval(() => {
+      this.count -= 1;
+      if (this.count <= 0) {
+        clearInterval(this.interval$);
+      }
+    }, 1000);
+  }
+
+  submit(): void {
+    const post = {
+      username: this.username,password: this.password
+    }
+    this.http.post('http://localhost:4000/auth/login',post).subscribe((res:any)=>{
+      const {token, user, expiresIn} = res.data;
+      const tokenExpires = Date.now() + expiresIn * 60 * 1000
+      // 本地存储
+      localStorage.setItem('token', token);
+    })
+  }
+
+  // #region social
+
+  open(type: string, openType: SocialOpenType = 'href'): void {
+    let url = ``;
+    let callback = ``;
+    if (environment.production) {
+      callback = `https://ng-alain.github.io/ng-alain/#/passport/callback/${type}`;
+    } else {
+      callback = `http://localhost:4200/#/passport/callback/${type}`;
+    }
+    switch (type) {
+      case 'auth0':
+        url = `//cipchk.auth0.com/login?client=8gcNydIDzGBYxzqV0Vm1CX_RXH-wsWo5&redirect_uri=${decodeURIComponent(callback)}`;
+        break;
+      case 'github':
+        url = `//github.com/login/oauth/authorize?client_id=9d6baae4b04a23fcafa2&response_type=code&redirect_uri=${decodeURIComponent(
+          callback
+        )}`;
+        break;
+      case 'weibo':
+        url = `https://api.weibo.com/oauth2/authorize?client_id=1239507802&response_type=code&redirect_uri=${decodeURIComponent(callback)}`;
+        break;
+    }
+    if (openType === 'window') {
+      this.socialService
+        .login(url, '/', {
+          type: 'window'
+        })
+        .subscribe(res => {
+          if (res) {
+            this.settingsService.setUser(res);
+            this.router.navigateByUrl('/');
+          }
+        });
+    } else {
+      this.socialService.login(url, '/', {
+        type: 'href'
+      });
+    }
+  }
+
+  // #endregion
+
+  ngOnDestroy(): void {
+    if (this.interval$) {
+      clearInterval(this.interval$);
+    }
+  }
+}
